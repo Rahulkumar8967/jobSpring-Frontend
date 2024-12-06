2519bc4a01fba0f86ffb44de9e631ed7157f2092
Rahul Saini <sainirahul8967@gmail.com>
2024-12-06 14:29:34 +0530

    add job page for student and recruiter but data not fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,81 @@
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 //import { Button } from './components/ui/button'
-import AppLayout from './layouts/app-layout'
-import LandingPage from './pages/landing'
-import Onboarding from './pages/onboarding';
-import JobListing from './pages/job-listing';
-import JobPage from './pages/job';
-import PostJob from './pages/post-job';
-import SavedJob from './pages/saved-jobs';
-import MyJobs from './pages/my-jobs';
-import { ThemeProvider } from './components/theme-provider';
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import JobListing from "./pages/job-listing";
+import JobPage from "./pages/job";
+import PostJob from "./pages/post-job";
+import SavedJob from "./pages/saved-jobs";
+import MyJobs from "./pages/my-jobs";
+import { ThemeProvider } from "./components/theme-provider";
+import ProtectedRoute from "./components/protected-route";
 
 const router = createBrowserRouter([
   {
-    element:<AppLayout />,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-      path:'/',
-      element:<LandingPage />
+        path: "/",
+        element: (
+            <LandingPage />
+        ),
       },
-      {
-        path:'/onboarding',
-        element:<Onboarding />
-        },
-        {
-          path:'/jobs',
-          element:<JobListing />
-          },
-          {
-            path:'/job/:id',
-            element:<JobPage />
-            },
-            {
-              path:'/post-job',
-              element:<PostJob />
-              },
-              {
-                path:'/saved-job',
-                element:<SavedJob />
-                },
-                {
-                  path:'/my-jobs',
-                  element:<MyJobs />
-                  },
 
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
+      },
+
+      {
+        path: "/jobs",
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+
+      {
+        path: "/job/:id",
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJob />
+          </ProtectedRoute>
+        ),
+      },
+
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ]);
