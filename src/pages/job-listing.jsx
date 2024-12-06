1830c66f268fd7942d38e129c72fdcc0d093befb
Rahul Saini <sainirahul8967@gmail.com>
2024-12-06 23:44:13 +0530

    remove token error

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,6 +1,26 @@
+import { getJobs } from "@/api/apiJobs";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 
 const JobListing = () => {
+
+const {session} = useSession();
+
+const fetchJobs = async () => {
+  const supabaseAccessToken = await session.getToken({
+    template:"supabase",
+  });
+  console.log("Supabase access",supabaseAccessToken)
+ const data = await getJobs(supabaseAccessToken);
+ console.log(data);
+ 
+};
+
+useEffect(()=>{
+ fetchJobs();
+},[]);
+
   return (
     <div>job-listing</div>
   )
--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,6 @@
-
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabaseClient = async (supabaseAccessToken) => {
@@ -13,4 +12,3 @@ const supabaseClient = async (supabaseAccessToken) => {
 };
 
 export default supabaseClient;
-        
